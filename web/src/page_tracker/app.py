cc3160af08d10ef51ca5ee23dfe3d0e9aae1b952
Mbongeni Mzila <mbongenimzilam@gmail.com>
2023-06-21 18:01:29 +0200

    Refactor folders and add docker compose

--- web/src/page_tracker/app.py
+++ web/src/page_tracker/app.py
@@ -16,8 +16,8 @@ def index():
         # page_views = redis.incr("page_views")
         page_views = redis().incr("page_views")
     except RedisError:
-        app.logger.exception("Redis error") # pylint: disable=E1101
-        return "Sprry, something went wrong \N{pensive face}", 500
+        app.logger.exception("Redis error")  # pylint: disable=E1101
+        return "Sorry, something went wrong \N{pensive face}", 500
     else:
         return f"This page has been seen {page_views} times."
 
@@ -26,5 +26,7 @@ def index():
 def redis():
     return Redis.from_url(os.getenv("REDIS_URL", "redis://localhost:6379"))
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    # app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run()
